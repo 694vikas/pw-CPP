2727c8c66f9bd54f63ed8fe56b6865c8c5d62e31
694vikas <er.vikasdubauli@gmail.com>
2024-11-16 12:06:23 +0530

    final

--- optimisedBubbleSort.cpp
+++ optimisedBubbleSort.cpp
@@ -19,7 +19,7 @@ int main()
             break;
     };
     cout << "sorted array is:";
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i <= n - 1; i++)
     {
         cout << a[i] << " ";
     }
